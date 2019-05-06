8994f74f987427941f28cb118500a7b6f85a7842
Henry Panora <hpanora@gmail.com>
2019-05-06 23:51:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@
 def position_taken?(board, index)
   board = []
   index = 0
-  (board[index] ==  " " || board[index] == "" || board[index] == nil || board[index] == "X" or "O") ?
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
       taken = true
 end
